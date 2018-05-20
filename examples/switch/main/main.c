380662d7d2c0f8738bb5c46c97bbd64db33db2e5
Tassilo Karge <tassilo.karge@web.de>
2018-05-20 13:48:37 +0200

    separated strands and white leds of strands to have more fine-grained control

--- examples/switch/main/main.c
+++ examples/switch/main/main.c
@@ -20,9 +20,13 @@
 #define TAG "LED STRIPE"
 #define PIN_CODE "123-58-197"
 
-#define ACCESSORY_NAME  "Deckenlampe"
+#define ACCESSORY_NAME  "Außenring"
+#define ACCESSORY_NAME2  "Weiß außen"
+#define ACCESSORY_NAME3  "Innenring"
+#define ACCESSORY_NAME4  "Weiß innen"
 #define MANUFACTURER_NAME   "TASSILO KARGE"
-#define MODEL_NAME  "ULTILED"
+#define MODEL_NAME  "ULTI_LED_Warmweiss"
+#define MODEL_NAME2  "ULTI_LED_Kaltweiss"
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
 
 #if 1
@@ -59,7 +63,7 @@ static void* a;
 static int led = true;
 static void* led_ev_handle;
 #define switch_flag 'I'
-static int brightness = 80;
+static int brightness = 70;
 static void* brightness_ev_handle;
 #define brightness_flag 'V'
 static int saturation = 6500;
@@ -68,6 +72,25 @@ static void* saturation_ev_handle;
 static int hue = 3900;
 static void* hue_ev_handle;
 #define hue_flag 'H'
+static int whiteled = 70;
+static void* whiteled_ev_handle;
+#define whiteled_flag 'W'
+
+static int led2 = true;
+static void* led_ev_handle2;
+#define switch_flag2 'i'
+static int brightness2 = 50;
+static void* brightness_ev_handle2;
+#define brightness_flag2 'v'
+static int saturation2 = 6500;
+static void* saturation_ev_handle2;
+#define saturation_flag2 's'
+static int hue2 = 3900;
+static void* hue_ev_handle2;
+#define hue_flag2 'h'
+static int whiteled2 = 50;
+static void* whiteled_ev_handle2;
+#define whiteled_flag2 'w'
 
 static strand_t strands[] = {
     {.rmtChannel = 0, .gpioNum = LED_STRIPE, .ledType = LED_SK6812W_V1, .brightLimit = 250, .numPixels = num_pixels,
@@ -79,11 +102,8 @@ static strand_t strands[] = {
 
 int STRANDCNT = sizeof(strands)/sizeof(strands[0]);
 
-void hsvToRGBW(float h, float s, float v, 
-    int *red, int *green, int *blue, int *white) {
-    
-    *white = max(((v - s) - 0.5) * 2 * 255, 0);
-
+void hsvToRGB(float h, float s, float v, 
+    int *red, int *green, int *blue) {
     double r = 0, g = 0, b = 0;
 
     if (s <= 0)
@@ -155,18 +175,42 @@ void hsvToRGBW(float h, float s, float v,
 void set_values_to_led() {
     int r, g, b, w;
     //saturation and brightness expected in values 0-1
-    hsvToRGBW(hue/100.0, saturation/100.0/100.0, (brightness*led)/100.0, &r, &g, &b, &w);
+    hsvToRGB(hue/100.0, saturation/100.0/100.0, (brightness*led)/100.0, &r, &g, &b);
 
     int brightLimit = strands[0].brightLimit;
     r = (r*r * brightLimit) / 65025;
     g = (g*g * brightLimit) / 65025;
     b = (b*b * brightLimit) / 65025;
-    w = (w*w * brightLimit) / 65025;
+    w = (whiteled * led * brightLimit) / 255;
 
-    printf("SET VALUES: ON: %d\nH: %.2f S: %.2f V: %d\nR: %d G: %d B: %d W: %d\n", 
-        led, hue/100.0, saturation/100.0, brightness, r, g, b, w);
+    printf("SET VALUES TO LED 1: ON: %d\nH: %.2f S: %.2f V: %d / R: %d G: %d B: %d W: %d\n", 
+    led, hue/100.0, saturation/100.0, brightness, r, g, b, w);
 
-    for (int i = 0; i < STRANDCNT; i++) {
+    strand_t *strand = &(strands[0]);
+
+    for (uint16_t i = 0; i < strand->numPixels; i++) {
+            strand->pixels[i] = pixelFromRGBW(
+                r,g,b,w);
+    }
+    digitalLeds_updatePixels(strand);
+    gpio_set_level(LED_PORT, led || led2 ? 1 : 0);
+}
+
+void set_values_to_led2() {
+    int r, g, b, w;
+    //saturation and brightness expected in values 0-1
+    hsvToRGB(hue2/100.0, saturation2/100.0/100.0, (brightness2*led2)/100.0, &r, &g, &b);
+
+    int brightLimit = strands[0].brightLimit;
+    r = (r*r * brightLimit) / 65025;
+    g = (g*g * brightLimit) / 65025;
+    b = (b*b * brightLimit) / 65025;
+    w = (whiteled2 * led2 * brightLimit) / 255;
+
+    printf("SET VALUES TO LED 2: ON: %d\nH: %.2f S: %.2f V: %d / R: %d G: %d B: %d W: %d\n", 
+        led2, hue2/100.0, saturation2/100.0, brightness2, r, g, b, w);
+
+    for (int i = 1; i < STRANDCNT; i++) {
         strand_t *strand = &(strands[i]);
 
         for (uint16_t i = 0; i < strand->numPixels; i++) {
@@ -175,13 +219,13 @@ void set_values_to_led() {
         }
         digitalLeds_updatePixels(strand);
     }
-    gpio_set_level(LED_PORT, led ? 1 : 0);
+    gpio_set_level(LED_PORT, led || led2 ? 1 : 0);
 }
 
 void* led_read(void* arg)
 {
     int c = (int) arg;
-    printf("[MAIN] LED READ %c \n", (char)c);
+    printf("[MAIN] LED 1 READ %c \n", (char)c);
     switch(c) {
         case switch_flag:
             return (void *)led;
@@ -191,6 +235,28 @@ void* led_read(void* arg)
             return (void *)hue;
         case saturation_flag:
             return (void *)saturation;
+        case whiteled_flag:
+            return (void *)whiteled;
+    }
+
+    return NULL;
+}
+
+void* led_read2(void* arg)
+{
+    int c = (int) arg;
+    printf("[MAIN] LED 2 READ %c \n", (char)c);
+    switch(c) {
+        case switch_flag2:
+            return (void *)led2;
+        case brightness_flag2:
+            return (void *)brightness2;
+        case hue_flag2:
+            return (void *)hue2;
+        case saturation_flag2:
+            return (void *)saturation2;
+        case whiteled_flag2:
+            return (void *)whiteled2;
     }
 
     return NULL;
@@ -214,6 +280,9 @@ void led_write(void* arg, void* value, int len)
             //hue is float, but does not come multiplied with 100 as expected
             saturation = (int) value * 100;
             break;
+        case whiteled_flag:
+            whiteled = (int) value;
+            break;
     }
 
     printf("[MAIN] LED WRITE. Arg: %c\n", (char)c);
@@ -236,6 +305,63 @@ void led_write(void* arg, void* value, int len)
         case saturation_flag:
             if (saturation_ev_handle)
                 hap_event_response(a, saturation_ev_handle, (void *)saturation);
+            break;
+        case whiteled_flag:
+            if (whiteled_ev_handle)
+                hap_event_response(a, whiteled_ev_handle, (void *)whiteled);
+            break;
+    }
+
+    return;
+}
+
+void led_write2(void* arg, void* value, int len)
+{
+    int c = (int) arg;
+    switch(c) {
+        case switch_flag2:
+            led2 = ((int) value) ? true : false;
+            break;
+        case brightness_flag2:
+            brightness2 = (int) value;
+            break;
+        case hue_flag2:
+            //hue is float, but does not come multiplied with 100 as expected
+            hue2 = (int) value * 100;
+            break;
+        case saturation_flag2:
+            //hue is float, but does not come multiplied with 100 as expected
+            saturation2 = (int) value * 100;
+            break;
+        case whiteled_flag2:
+            whiteled2 = (int) value;
+            break;
+    }
+
+    printf("[MAIN] LED WRITE. Arg: %c\n", (char)c);
+
+    set_values_to_led2();
+
+    switch(c) {
+        case switch_flag2:
+            if (led_ev_handle2)
+                hap_event_response(a, led_ev_handle2, (void *)led2);
+            break;
+        case brightness_flag2:
+            if (brightness_ev_handle2)
+                hap_event_response(a, brightness_ev_handle2, (void *)brightness2);
+            break;
+        case hue_flag2:
+            if (hue_ev_handle2)
+                hap_event_response(a, hue_ev_handle2, (void *)hue2);
+            break;
+        case saturation_flag2:
+            if (saturation_ev_handle2)
+                hap_event_response(a, saturation_ev_handle2, (void *)saturation2);
+            break;
+        case whiteled_flag2:
+            if (whiteled_ev_handle)
+                hap_event_response(a, whiteled_ev_handle2, (void *)whiteled2);
             break;
     }
 
@@ -261,6 +387,8 @@ void led_notify(void* arg, void* ev_handle, bool enable)
         case saturation_flag:
             saturation_ev_handle = ev_handle;
             break;
+        case whiteled_flag:
+            whiteled_ev_handle = ev_handle;
         }
     } else {
         switch(c) {
@@ -276,6 +404,50 @@ void led_notify(void* arg, void* ev_handle, bool enable)
         case saturation_flag:
             saturation_ev_handle = NULL;
             break;
+        case whiteled_flag:
+            whiteled_ev_handle = NULL;
+        }
+    }
+}
+
+void led_notify2(void* arg, void* ev_handle, bool enable)
+{
+    int c = (int) arg;
+    printf("[MAIN] LED NOTIFY %c \n", (char)c);   
+    
+    if (enable) {
+        switch(c) {
+        case switch_flag2:
+            led_ev_handle2 = ev_handle;
+            break;
+        case brightness_flag2:
+            brightness_ev_handle2 = ev_handle;
+            break;
+        case hue_flag2:
+            hue_ev_handle2 = ev_handle;
+            break;
+        case saturation_flag2:
+            saturation_ev_handle2 = ev_handle;
+            break;
+        case whiteled_flag2:
+            whiteled_ev_handle2 = ev_handle;
+        }
+    } else {
+        switch(c) {
+        case switch_flag2:
+            led_ev_handle2 = NULL;
+            break;
+        case brightness_flag2:
+            brightness_ev_handle2 = NULL;
+            break;
+        case hue_flag2:
+            hue_ev_handle2 = NULL;
+            break;
+        case saturation_flag2:
+            saturation_ev_handle2 = NULL;
+            break;
+        case whiteled_flag2:
+            whiteled_ev_handle2 = NULL;
         }
     }
 }
@@ -293,24 +465,83 @@ void hap_object_init(void* arg)
 {
     void* accessory_object = hap_accessory_add(a);
     
-    struct hap_characteristic cs[] = {
+    struct hap_characteristic cs1[] = {
         {HAP_CHARACTER_IDENTIFY, (void*)true, NULL, identify_read, NULL, NULL},
         {HAP_CHARACTER_MANUFACTURER, (void*)MANUFACTURER_NAME, NULL, NULL, NULL, NULL},
         {HAP_CHARACTER_MODEL, (void*)MODEL_NAME, NULL, NULL, NULL, NULL},
         {HAP_CHARACTER_NAME, (void*)ACCESSORY_NAME, NULL, NULL, NULL, NULL},
         {HAP_CHARACTER_SERIAL_NUMBER, (void*)"0123456789", NULL, NULL, NULL, NULL},
-        {HAP_CHARACTER_FIRMWARE_REVISION, (void*)"1.1", NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_FIRMWARE_REVISION, (void*)"1.3", NULL, NULL, NULL, NULL},
     };
 
-    hap_service_and_characteristics_add(a, accessory_object, HAP_SERVICE_ACCESSORY_INFORMATION, cs, ARRAY_SIZE(cs));
+    hap_service_and_characteristics_add(a, accessory_object, HAP_SERVICE_ACCESSORY_INFORMATION, cs1, ARRAY_SIZE(cs1));
 
-    struct hap_characteristic cc[] = {
+    struct hap_characteristic cc1[] = {
         {HAP_CHARACTER_ON, (void*)led, (void*)switch_flag, led_read, led_write, led_notify},
         {HAP_CHARACTER_BRIGHTNESS, (void*)brightness, (void*)brightness_flag, led_read, led_write, led_notify},
         {HAP_CHARACTER_HUE, (void*)hue, (void*)hue_flag, led_read, led_write, led_notify},
         {HAP_CHARACTER_SATURATION, (void*)saturation, (void*)saturation_flag, led_read, led_write, led_notify}
     };
-    hap_service_and_characteristics_add(a, accessory_object, HAP_SERVICE_LIGHTBULB, cc, ARRAY_SIZE(cc));
+    hap_service_and_characteristics_add(a, accessory_object, HAP_SERVICE_LIGHTBULB, cc1, ARRAY_SIZE(cc1));
+
+    void* accessory_object2 = hap_accessory_add(a);
+
+    struct hap_characteristic cs2[] = {
+        {HAP_CHARACTER_IDENTIFY, (void*)true, NULL, identify_read, NULL, NULL},
+        {HAP_CHARACTER_MANUFACTURER, (void*)MANUFACTURER_NAME, NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_MODEL, (void*)MODEL_NAME, NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_NAME, (void*)ACCESSORY_NAME2, NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_SERIAL_NUMBER, (void*)"0123456789", NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_FIRMWARE_REVISION, (void*)"1.3", NULL, NULL, NULL, NULL},
+    };
+
+    hap_service_and_characteristics_add(a, accessory_object2, HAP_SERVICE_ACCESSORY_INFORMATION, cs2, ARRAY_SIZE(cs2));
+
+    struct hap_characteristic cc2[] = {
+        {HAP_CHARACTER_ON, (void*)led, (void*)switch_flag, led_read, led_write, led_notify},
+        {HAP_CHARACTER_BRIGHTNESS, (void*)whiteled, (void*)whiteled_flag, led_read, led_write, led_notify}
+    };
+    hap_service_and_characteristics_add(a, accessory_object2, HAP_SERVICE_LIGHTBULB, cc2, ARRAY_SIZE(cc2));
+
+    void* accessory_object3 = hap_accessory_add(a);
+    
+    struct hap_characteristic cs3[] = {
+        {HAP_CHARACTER_IDENTIFY, (void*)true, NULL, identify_read, NULL, NULL},
+        {HAP_CHARACTER_MANUFACTURER, (void*)MANUFACTURER_NAME, NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_MODEL, (void*)MODEL_NAME2, NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_NAME, (void*)ACCESSORY_NAME3, NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_SERIAL_NUMBER, (void*)"0123456789", NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_FIRMWARE_REVISION, (void*)"1.3", NULL, NULL, NULL, NULL},
+    };
+
+    hap_service_and_characteristics_add(a, accessory_object3, HAP_SERVICE_ACCESSORY_INFORMATION, cs3, ARRAY_SIZE(cs3));
+
+    struct hap_characteristic cc3[] = {
+        {HAP_CHARACTER_ON, (void*)led2, (void*)switch_flag2, led_read2, led_write2, led_notify2},
+        {HAP_CHARACTER_BRIGHTNESS, (void*)brightness2, (void*)brightness_flag2, led_read2, led_write2, led_notify2},
+        {HAP_CHARACTER_HUE, (void*)hue2, (void*)hue_flag2, led_read2, led_write2, led_notify2},
+        {HAP_CHARACTER_SATURATION, (void*)saturation2, (void*)saturation_flag2, led_read2, led_write2, led_notify2}
+    };
+    hap_service_and_characteristics_add(a, accessory_object3, HAP_SERVICE_LIGHTBULB, cc3, ARRAY_SIZE(cc3));
+
+    void* accessory_object4 = hap_accessory_add(a);
+
+    struct hap_characteristic cs4[] = {
+        {HAP_CHARACTER_IDENTIFY, (void*)true, NULL, identify_read, NULL, NULL},
+        {HAP_CHARACTER_MANUFACTURER, (void*)MANUFACTURER_NAME, NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_MODEL, (void*)MODEL_NAME2, NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_NAME, (void*)ACCESSORY_NAME4, NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_SERIAL_NUMBER, (void*)"0123456789", NULL, NULL, NULL, NULL},
+        {HAP_CHARACTER_FIRMWARE_REVISION, (void*)"1.3", NULL, NULL, NULL, NULL},
+    };
+
+    hap_service_and_characteristics_add(a, accessory_object4, HAP_SERVICE_ACCESSORY_INFORMATION, cs4, ARRAY_SIZE(cs4));
+
+    struct hap_characteristic cc4[] = {
+        {HAP_CHARACTER_ON, (void*)led2, (void*)switch_flag2, led_read2, led_write2, led_notify2},
+        {HAP_CHARACTER_BRIGHTNESS, (void*)whiteled2, (void*)whiteled_flag2, led_read2, led_write2, led_notify2}
+    };
+    hap_service_and_characteristics_add(a, accessory_object4, HAP_SERVICE_LIGHTBULB, cc4, ARRAY_SIZE(cc4));
 }
 
 
@@ -395,6 +626,7 @@ void app_main()
 
     //set defaults when powering on
     set_values_to_led();
-    
+    set_values_to_led2();
+
     wifi_init_sta();
 }
